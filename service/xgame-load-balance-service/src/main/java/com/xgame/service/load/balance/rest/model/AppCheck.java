18be1c6d5c3969ecf89c0622b4b6829f2dc6203e
zhucw <zhucw@kingnet.com>
2017-11-21 22:11:48 +0800

    add apple check flag

---
+++ service/xgame-load-balance-service/src/main/java/com/xgame/service/load/balance/rest/model/AppCheck.java
@@ -0,0 +1,13 @@
+package com.xgame.service.load.balance.rest.model;
+
+public class AppCheck {
+    private Integer checkFlag;
+
+    public Integer getCheckFlag() {
+        return checkFlag;
+    }
+
+    public void setCheckFlag(Integer checkFlag) {
+        this.checkFlag = checkFlag;
+    }
+}
