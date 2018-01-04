b04a6673630c38a53fc58ab46f02a0aa714eccc7
lkyprogramer <luokaiyao2012@gmail.com>
2018-01-04 21:40:14 +0800

    add new field

--- service/xgame-load-balance-service/src/main/java/com/xgame/service/load/balance/db/dao/ServerDao.java
+++ service/xgame-load-balance-service/src/main/java/com/xgame/service/load/balance/db/dao/ServerDao.java
@@ -7,5 +7,5 @@ import java.util.List;
 public interface ServerDao extends BaseDao {
 
     public ServerDto getServerInfoById(Integer serverId);
-    public List<ServerDto> getActiveServer();
+    public List<ServerDto> getActiveServer(String ver,String plaform);
 }
--- service/xgame-load-balance-service/src/main/java/com/xgame/service/load/balance/rest/model/UserInfo.java
+++ service/xgame-load-balance-service/src/main/java/com/xgame/service/load/balance/rest/model/UserInfo.java
@@ -2,6 +2,24 @@ package com.xgame.service.load.balance.rest.model;
 
 public class UserInfo {
     private String userName;
+    private String ver;
+    private String platform;
+
+    public String getVer() {
+        return ver;
+    }
+
+    public void setVer(String ver) {
+        this.ver = ver;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
 
     public String getUserName() {
         return userName;
@@ -9,5 +27,10 @@ public class UserInfo {
 
     public void setUserName(String userName) {
         this.userName = userName;
+    }
+
+    @Override
+    public String toString(){
+        return "useName="+userName+" version="+ver+" platform="+platform;
     }
 }
